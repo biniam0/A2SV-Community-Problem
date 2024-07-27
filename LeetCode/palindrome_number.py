27881a9c2bd3beaf5280d8f6757d393d89c708ff
biniam0 <153349728+biniam0@users.noreply.github.com>
2024-07-26 23:27:16 -0900

    Update palindrome_number.py

--- LeetCode/palindrome_number.py
+++ LeetCode/palindrome_number.py
@@ -1,7 +1,8 @@
-def isPalindrome(self, x: int) -> bool:
-    if x < 0:
-        return False
-    if x == int(str(x)[::-1]):
-        return True
-    else:
-        return False
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        if x == int(str(x)[::-1]):
+            return True
+        else:
+            return False
